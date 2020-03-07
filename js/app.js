e4e60e919ab7b4006a748268b96e10b8d645fb17
Denys Horopashnyi <gdi1991@gmail.com>
2020-03-07 13:44:31 +0200

    almost finish behavior of .toggleAsideBtn

--- js/app.js
+++ js/app.js
@@ -4,13 +4,16 @@ const aside = document.getElementsByClassName('aside')[0];
 const subheader = document.getElementsByClassName('subheader')[0];
 const toggleAsideBtn = document.getElementsByClassName('header__item--show')[0];
 const asideNavText = document.getElementsByClassName('aside-nav__text');
+const burger = document.getElementsByClassName('burger')[0];
 
 toggleAsideBtn.addEventListener('click', toggleAsideState);
 
 function toggleAsideState() {
     aside.classList.toggle('hidden');
     subheader.classList.toggle('hidden');
-    toggleAsideBtn.classList.toggle('header__item--active');
+    toggleAsideBtn.classList.toggle('header__item--clicked');
+    burger.classList.toggle('hidden');
+    // burger.classList.toggle('show');
     for (const prop in asideNavText) {
         asideNavText[prop].classList.toggle('hidden');
     }
